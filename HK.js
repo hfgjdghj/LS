bdbf24005354387276677ccbcd104ea207e2082a
hfgjdghj <43291684+hfgjdghj@users.noreply.github.com>
2019-01-02 15:23:33 +0800

    Add files via upload

--- HK.js
+++ HK.js
@@ -1,6 +1,6 @@
 function proceed(){
  var x = document.getElementById('pass').value;
     if (x=="123"){
-        window.location.href = "https://hfgjdghj.github.io/LS/HK concepts.html";
+        window.location.href = "https://hfgjdghj.github.io/LS/HK_concepts.html";
     }
 }
